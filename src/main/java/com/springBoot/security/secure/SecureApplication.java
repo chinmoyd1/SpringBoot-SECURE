4e4cdc629dfa86f4017648c3c139fddc5151e75c
chinmoyd1 <chinmoy1113@gmail.com>
2019-05-05 22:03:33 +0530

    first commit

---
+++ src/main/java/com/springBoot/security/secure/SecureApplication.java
@@ -0,0 +1,14 @@
+package com.springBoot.security.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class SecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureApplication.class, args);
+	}
+
+}
---
+++ src/main/webapp/welcome.jsp
@@ -0,0 +1,5 @@
+<html>
+<body>
+Im a Alien...............
+</body>
+</html>
